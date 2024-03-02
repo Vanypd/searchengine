dc9374c583543d9d2f8aa8c037f0401af7d6d6ae
Ivan Pudnik <pk.van@mail.ru>
2024-03-02 16:18:39 +0300

    Unused methods deleted

--- src/main/java/searchengine/repository/implementation/PageRepository.java
+++ src/main/java/searchengine/repository/implementation/PageRepository.java
@@ -1,16 +1,7 @@
 package searchengine.repository.implementation;
 
-import org.springframework.data.jpa.repository.Query;
 import searchengine.model.implementation.Page;
 import searchengine.repository.GenericRepository;
 
-import java.util.List;
-
 public interface PageRepository extends GenericRepository<Page> {
-
-    @Query("SELECT p.path FROM Page p WHERE p.siteId.url = :url")
-    List<String> findAllPathsBySiteUrl(String url);
-
-    @Query("SELECT p.path FROM Page p")
-    List<String> getAllPaths();
 }
